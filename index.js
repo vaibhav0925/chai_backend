7c7ee8df43913a08a0e7792d0f95d0e811da73a1
vaibhva0925 <vaibha0925@gmail.com>
2024-02-29 19:37:52 +0530

    github data added

--- index.js
+++ index.js
@@ -8,6 +8,41 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+const githubData = {
+  "login": "vaibhav0925",
+  "id": 124309121,
+  "node_id": "U_kgDOB2jOgQ",
+  "avatar_url": "https://avatars.githubusercontent.com/u/124309121?v=4",
+  "gravatar_id": "",
+  "url": "https://api.github.com/users/vaibhav0925",
+  "html_url": "https://github.com/vaibhav0925",
+  "followers_url": "https://api.github.com/users/vaibhav0925/followers",
+  "following_url": "https://api.github.com/users/vaibhav0925/following{/other_user}",
+  "gists_url": "https://api.github.com/users/vaibhav0925/gists{/gist_id}",
+  "starred_url": "https://api.github.com/users/vaibhav0925/starred{/owner}{/repo}",
+  "subscriptions_url": "https://api.github.com/users/vaibhav0925/subscriptions",
+  "organizations_url": "https://api.github.com/users/vaibhav0925/orgs",
+  "repos_url": "https://api.github.com/users/vaibhav0925/repos",
+  "events_url": "https://api.github.com/users/vaibhav0925/events{/privacy}",
+  "received_events_url": "https://api.github.com/users/vaibhav0925/received_events",
+  "type": "User",
+  "site_admin": false,
+  "name": "Vaibhav Shukla",
+  "company": null,
+  "blog": "",
+  "location": null,
+  "email": null,
+  "hireable": null,
+  "bio": null,
+  "twitter_username": null,
+  "public_repos": 5,
+  "public_gists": 0,
+  "followers": 0,
+  "following": 3,
+  "created_at": "2023-02-02T20:47:36Z",
+  "updated_at": "2024-02-29T13:43:27Z"
+}
+
 app.get('/twitter', (req, res) => {
     res.send('vaibhavdotcom')
 })
@@ -18,6 +53,10 @@ app.get('/logIn', (req, res) => {
 
 app.get('/youtube', (req, res) => {
     res.send('<h2>Chai Aur Code</h2>')
+})
+
+app.get('/github', (req, res) => {
+  res.json(githubData)
 })
 
 app.listen(process.env.PORT, () => {
